d4a6cae31953c09351652d2706e9211f2ee4d22e
Arjun Patel <arjunpatel@Arjuns-MacBook-Pro.local>
2020-09-16 20:38:45 -0400

    initial version upload to github

---
+++ src/test/java/com/arjun/aws/beanstalk/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.aws.beanstalk.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
